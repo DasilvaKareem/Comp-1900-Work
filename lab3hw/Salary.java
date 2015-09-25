ce2e81c505a69006e78dd7e944e8b4895fb02c80
Kareem Dasilva <kdasilva@memphis.edu>
2015-09-25 12:01:49 -0500

    changed lab 3 hw

--- lab3hw/Salary.java
+++ lab3hw/Salary.java
@@ -2,8 +2,8 @@
 /**
  * Write a description of class Salary here.
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author Kareem Dasilva 
+ * @version RASENSHIRUKEN
  */
 import java.util.*;
 public class Salary
